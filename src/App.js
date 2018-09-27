1a1869fb3b7d5e328b4fcda7d248f8a4da4639f0
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2018-09-26 23:04:18 -0700

    updated to use material ui. added appbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import FriendCard from "./components/FriendCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
+import PrimarySearchAppBar from "./components/PrimarySearchAppBar";
 import friends from "./friends.json";
 import "./App.css";
 
@@ -18,20 +19,45 @@ class App extends Component {
     this.setState({ friends });
   };
 
+  createStructure = friends => {
+    function createSignsArr(friends) {
+      const signs = []
+      console.log(signs)
+      for (let i = 1; i <= friends / 2; i++) {
+        signs.push(i, i)
+      }
+      return signs
+    }
+  
+    const signsDouble = createSignsArr(friends)
+  
+    const container = document.createElement('div')   
+    container.setAttribute('className', 'container')
+    document.body.appendChild(container)
+    for (let i = 0; i < friends; i++) {
+      let div = document.createElement('div')
+      div.setAttribute('className', 'tile')
+      div.setAttribute('id', `tile${i}`)
+      let randomNum = Math.floor(Math.random() * signsDouble.length)
+      let tile = signsDouble.splice(randomNum, 1)[0]
+      div.innerHTML = tile
+      container.appendChild(div)
+    }
+  }
+
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title>Friends List</Title>
+        <PrimarySearchAppBar />
+        <Title>Clicky-Hawks</Title>
         {this.state.friends.map(friend => (
           <FriendCard
             removeFriend={this.removeFriend}
             id={friend.id}
             key={friend.id}
-            name={friend.name}
             image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
+            picked={friend.picked}
           />
         ))}
       </Wrapper>
