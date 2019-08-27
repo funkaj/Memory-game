2f96b3758a36b8e0eb00008444f0a818fcd7cee9
Aramoro <35650608+Yogo-Isoroku@users.noreply.github.com>
2019-08-27 12:55:45 -0700

    reformatted all files. removed empty files

--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -1,13 +1,12 @@
-import React from "react";
-import "./FriendCard.css";
+import React from 'react';
+import './FriendCard.css';
 
 const FriendCard = props => (
-    <div className='tile'>
-    <div className='img-container'>
-      <img alt={props.name} src={props.image} />
-    </div>
-    </div>
+	<div className='tile'>
+		<div className='img-container'>
+			<img alt={props.name} src={props.image} />
+		</div>
+	</div>
 );
-
 
 export default FriendCard;
